1d97e9186bab09bb4809bfbc843c0d48ce097b82
sakshikarmukale-hub <sakshikarmukale@gmail.com>
2026-05-05 10:33:13 +0530

    change  1

--- login/src/Login.jsx
+++ login/src/Login.jsx
@@ -4,7 +4,7 @@ const Login = () => {
   return (
     <div>
       Hello Shubhangi We have to work on the same project 
-      <div style={{Height:200, width:200, color:"pink",border:"2px white solid", display:"flex"}}>
+      <div style={{Height:200, width:200, color:"Red",border:"2px white solid", display:"flex"}}>
          hello Shubha
       </div>
     </div>
